fa86c02f7ca50e4a555d208db3c46260f96c6d86
oreoluwa <akintomowooreoluwa@gmail.com>
2023-09-19 06:56:18 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdarg.h>
+
+
+int char_print(char c);
+
+int number_printer(int i);
+
+int string_print(char *p);
+
+int _printf(const char *format, ...);
+
+#endif
+
+
